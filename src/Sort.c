a7ce8e90cb61d4571ae88fa69b7e46f4d7df541c
kitosek <dolggik@gmail.com>
2018-06-26 22:31:28 +0700

    Modify sort.c

--- src/Sort.c
+++ src/Sort.c
@@ -3,11 +3,13 @@
 void sort(int n, char A[1200][1200])
 {
   int k;
+  int i;
+  int j;
   char temp[1200];   
-  for (int i = 0; i <= n - 2; i++) 
+  for ( i = 0; i <= n - 2; i++) 
   {
     k = i;
-    for(int j = i + 1; j <= n - 1; j++)
+    for( j = i + 1; j <= n - 1; j++)
     {
       if(strcasecmp(A[j], A[k]) < 0)
         k = j;        
